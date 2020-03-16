6ae2b9e0eac5fa9e24ed0f599f24d87a8b5ccdbd
Kristina Goncharova <kris.goncharova@inbox.ru>
2020-03-16 17:22:36 +0300

    validations added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,8 +14,11 @@ class EventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id])
-    @event.update(event_params)
-    redirect_to @event
+    if @event.update(event_params)
+      redirect_to @event, notice: "Event successfully updated"
+    else
+      render :edit
+    end
   end
 
   def new
@@ -24,8 +27,11 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
-    @event.save
-    redirect_to @event
+    if @event.save
+      redirect_to @event, notice: "Event successfully created"
+    else
+      render :new
+    end
   end
 
   def destroy
