471381a5893c7b86eff585d89631c5afcb27d397
Kristina Goncharova <kris.goncharova@inbox.ru>
2020-03-16 14:43:21 +0300

    added ability to delete events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -28,6 +28,12 @@ class EventsController < ApplicationController
     redirect_to @event
   end
 
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_url
+  end
+
   private
 
   def event_params
