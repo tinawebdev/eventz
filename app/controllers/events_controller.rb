9a468b07578e41e17d23013fb3469e926a028678
Kristina Goncharova <kris.goncharova@inbox.ru>
2020-03-16 14:15:52 +0300

    added ability to create and update events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,30 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    redirect_to @event
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to @event
+  end
+
+  private
+
+  def event_params
+    params.require(:event).
+      permit(:name, :description, :location, :price, :starts_at)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root "events#index"
 
-  get "events" => "events#index"
-  get "events/:id" => "events#show", as: "event"
+  resources :events
 end
