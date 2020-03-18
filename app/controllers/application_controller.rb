53689fa89e98f6452aaed00ca0a30baaa94e1617
Kristina Goncharova <kris.goncharova@inbox.ru>
2020-03-18 18:20:25 +0300

    admin added to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,23 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user?
 
+  def current_user_admin?
+    current_user && current_user.admin?
+  end
+
+  helper_method :current_user_admin?
+
   def require_signin
     unless current_user
       session[:intended_url] = request.url
       redirect_to new_session_url, alert: "Please, sing in first!"
     end
   end
+
+  def require_admin
+    unless current_user_admin?
+      redirect_to events_url, alert: "Unauthorized access!"
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_091805) do
+ActiveRecord::Schema.define(version: 2020_03_18_135653) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_03_17_091805) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "registrations", "events"
