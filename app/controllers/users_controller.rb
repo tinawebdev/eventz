91f278fc1c4b1dc6d8e246a8d76351c2275be936
Kristina Goncharova <kris.goncharova@inbox.ru>
2020-03-18 16:52:36 +0300

    user authentication and authorization added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
+
   def index
     @users = User.all
   end
@@ -14,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: "Thanks for signing up!"
     else
       render :new
@@ -21,11 +25,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: "Account successfully updated!"
     else
@@ -34,12 +36,17 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
+    session[:user_id] = nil
     redirect_to events_url, alert: "Account successfully deleted!"
   end
 
   private
+
+  def require_correct_user
+    @user = User.find(params[:id])
+    redirect_to events_url unless current_user?(@user)
+  end
 
   def user_params
     params.require(:user).
