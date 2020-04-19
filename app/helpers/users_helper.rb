421f541c9bc5455b020798d3d4ea81279984a58d
Kristina <codingkris@yahoo.com>
2020-04-19 12:56:40 +0300

    Added gravatar to users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravatar_for(user, styles: "rounded")
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: styles)
+  end
 end
